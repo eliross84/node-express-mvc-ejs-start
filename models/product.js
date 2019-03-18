62ccc5212f31fecea7b0a48fe8099586929a5635
Jacob Hughes <s527770@nwmissouri.edu>
2019-03-18 12:09:34 -0500

    added amount in store to product.js
    Signed-off-by: Jacob Hughes <s527770@nwmissouri.edu>

--- models/product.js
+++ models/product.js
@@ -9,11 +9,15 @@
 // see <https://mongoosejs.com/> for more information
 const mongoose = require('mongoose')
 
+//briar will change this
+
 const ProductSchema = new mongoose.Schema({
   _id: { type: Number, required: true },
   productKey: { type: String, required: true, unique: true, default: 'product' },
   description: { type: String, required: false, default: 'description' },
-  unitPrice: { type: Number, required: true, default: 9.99, min: 0, max: 100000 }
+  unitPrice: { type: Number, required: true, default: 9.99, min: 0, max: 100000 },
+  stock: {type: Number, required: true, default: 0, min: 0, max: 100000}
+  //add in an amount we have in store
 })
 
 module.exports = mongoose.model('Product', ProductSchema)
