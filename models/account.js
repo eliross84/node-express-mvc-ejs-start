015fa7b42497a310e19f17dcf70c8298c6189a1c
Jacob Hughes <s527770@nwmissouri.edu>
2019-03-22 11:47:49 -0500

    added account stuff
    Signed-off-by: Jacob Hughes <s527770@nwmissouri.edu>

---
+++ models/account.js
@@ -0,0 +1,14 @@
+/**
+Account MOdel
+Describes the characteristic of an acount (balance, number, routing number, etc)
+@author Jacob Briar Hughes <s527770@nwmissouri.edu>
+*/
+const mongoose = require('mongoose')
+
+const OrderSchema = new mongoose.Schema({
+    _id: {type: Number, required: true, unique: true},
+    acountNum: {type: Number, required: true},
+    balance: {type: Number, required: true},
+    routingNum: {type: Number, required: true, unique: true}
+})
+module.exports = mongoose.model('Account', OrderSchema)
