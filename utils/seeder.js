60a65a1cabb2596341afa8be47b4eadfaff8cf6f
eliross84 <eliross84@gmail.com>
2019-03-22 11:35:48 -0500

    Done with users model
    Signed-off-by: eliross84 <eliross84@gmail.com>

--- utils/seeder.js
+++ utils/seeder.js
@@ -9,7 +9,7 @@ const products = require('../data/products.json')
 const orders = require('../data/orders.json')
 const orderLineItems = require('../data/orderLineItems.json')
 const puppies = require('../data/puppies.json')
-const developers = require('../data/developers.json')
+const users = require('../data/users.json')
 
 
 
@@ -75,6 +75,18 @@ module.exports = (app) => {
   // initialize app.locals (these objects will be available to our controllers)
   app.locals.puppies = db.puppies.find(puppies)
   LOG.debug(`${app.locals.puppies.query.length} puppies seeded`)
+
+  //Users seeding
+  db.users = new Datastore()
+  db.users.loadDatabase()
+
+  // insert the sample data into our data store
+  db.users.insert(users)
+
+  // initialize app.locals (these objects will be available to our controllers)
+  app.locals.users = db.users.find(users)
+  LOG.debug(`${app.locals.users.query.length} users seeded`)
+
 
   LOG.info('END Seeder. Sample data read and verified.')
 }
